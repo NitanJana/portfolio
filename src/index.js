e796553df8c09b17608cc740728a4db59d18045f
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-12-04 18:56:10 +0530

    feat: add navbar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./stylesheets/styles.css";
+
+// function to change active class
+function changeActive(selectedItem) {
+  const navItems = document.querySelectorAll(".nav-item");
+  navItems.forEach((item) => item.classList.remove("active"));
+  selectedItem.classList.add("active");
+
+}
+
+const navItems = document.querySelectorAll(".nav-item");
+navItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    changeActive(item);
+  });
+});
