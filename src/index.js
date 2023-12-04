40fef519a2b6fc82ca5ed9de5c219b6af3640524
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-12-04 17:27:09 +0530

    refactor: remove redundant code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
 import "./stylesheets/styles.css";
-
-const element = document.getElementsByTagName("html")[0];
-element.style.display = "none";
-document.addEventListener("DOMContentLoaded", () => {
-  element.style.display = "block";
-});
